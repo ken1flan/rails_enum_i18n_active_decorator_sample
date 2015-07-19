82d9da87b07892c2dd1b9d6eb25a3830d54e4dfe
ken1flan <ken1flan@gmail.com>
2015-07-19 16:01:24 +0900

    Add status labels of article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,19 @@
 class Article < ActiveRecord::Base
+  STATUS_DRAFT = 0
+  STATUS_PUBLIC = 1
+  STATUS_DISABLED_BY_ADMIN = 2
+
+  STATUS_LABELS = {
+    STATUS_DRAFT => "下書き",
+    STATUS_PUBLIC => "公開中",
+    STATUS_DISABLED_BY_ADMIN => "管理者により非公開中",
+  }
+
+  def active?
+    status == STATUS_PUBLIC
+  end
+
+  def status_label
+    STATUS_LABELS[status]
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft article should not be active" do
+    draft_article = Article.new(title: "test", content: "content", status: Article::STATUS_DRAFT)
+    assert_not draft_article.active?
+  end
+
+  test "public article should be active" do
+    public_article = Article.new(title: "test", content: "content", status: Article::STATUS_PUBLIC)
+    assert public_article.active?
+  end
+
+  test "article disabled by admin should not be active" do
+    article_disabled_by_admin = Article.new(title: "test", content: "content", status: Article::STATUS_DISABLED_BY_ADMIN)
+    assert_not article_disabled_by_admin.active?
+  end
 end
